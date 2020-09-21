d07cc41b2c99941795e2d53f7b09a8a25e2360ea
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-21 16:29:38 +0530

    Update ContactMapper.java

--- src/main/java/com/example/demo/model/mapper/ContactMapper.java
+++ src/main/java/com/example/demo/model/mapper/ContactMapper.java
@@ -12,8 +12,7 @@ public class ContactMapper implements RowMapper<Contact> {
 	@Override
 	public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
 		Contact contact = Contact.builder().Id(rs.getString("Id"))
-				.firstName(rs.getString("firstName")).lastName(rs.getString("lastName"))
-				.phone(rs.getString("phone")).build();
+				.firstName(rs.getString("firstName")).build();
 				
 		return contact;
 	}
